df0cbb2d28e824cdbcaffa64924ea273cb82ad97
matrakei <48519913@est.ort.edu.ar>
2024-05-27 09:03:04 -0300

    LoginManager script checkpassword

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoginManager : MonoBehaviour
 {
+
+    public string contrasenia = manzana;
+    public InputField inputPassword;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void CheckPassword()
+    {
+        if (inputPassword.text == contrasenia)
+        {
+            Debug.Log("Access granted");
+        }
+        else 
+        {
+            Debug.Log("Access denied");
+        }
+    }
+
+
+
 }
